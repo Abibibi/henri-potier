371ab4293630d1b560ccbf001f7dac774924eab9
Abibibi <abeba.ngwe@gmail.com>
2020-10-08 15:43:11 +0200

    add to or remove from cart

--- src/components/HomeProduct/index.js
+++ src/components/HomeProduct/index.js
@@ -7,13 +7,48 @@ const HomeProduct = ({
   cover,
   price,
   title,
-}) => (
-  <HomeProductStyled>
-    <img src={cover} alt={title} />
-    <div>{price}€</div>
-    <h3>{title}</h3>
-    <button type="submit">Ajouter au panier</button>
-  </HomeProductStyled>
-);
+  cartChanges,
+  cartProducts,
+}) => {
+  const addRemoveProduct = ({ target: { id: clickedProductTitle } }) => {
+    let updatedCartProducts;
+
+    console.log('clicked title' + clickedProductTitle);
+
+    if (cartProducts.length) {
+      cartProducts.map((titleToSeekFor) => {
+        console.log('title to seek for' + titleToSeekFor);
+        if (clickedProductTitle === titleToSeekFor) {
+          updatedCartProducts = cartProducts.filter((titleToKeep) => titleToKeep !== clickedProductTitle);
+          console.log('removed products' + updatedCartProducts);
+        }
+        else if (clickedProductTitle !== titleToSeekFor) {
+          updatedCartProducts = [...cartProducts, clickedProductTitle];
+          console.log('added products' + updatedCartProducts);
+        }
+        updatedCartProducts = cartProducts.filter((titleToKeep) => titleToKeep !== titleToKeep);
+        return updatedCartProducts;
+      });
+    }
+    else {
+      updatedCartProducts = [clickedProductTitle];
+    }
+
+    cartChanges(updatedCartProducts);
+  };
+
+  const productInCart = cartProducts ? cartProducts.find(({ title: productTitle }) => productTitle === title) : false;
+
+  const cartButtonText = productInCart ? 'Retirer du panier' : 'Ajouter au panier';
+
+  return (
+    <HomeProductStyled>
+      <img src={cover} alt={title} />
+      <div>{price}€</div>
+      <h3>{title}</h3>
+      <button id={title} type="submit" onClick={addRemoveProduct}>{cartButtonText}</button>
+    </HomeProductStyled>
+  );
+};
 
 export default HomeProduct;
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,12 +2,14 @@
 const initialState = {
   homeProducts: [],
   search: '',
+  cartProducts: [],
 };
 
 // == Types
 export const HOME_PRODUCTS_SOUGHT = 'HOME_PRODUCTS_SOUGHT';
 const HOME_PRODUCTS_FETCHED = 'HOME_PRODUCTS_FETCHED';
 const SEARCH_INPUT_CHANGED = 'SEARCH_INPUT_CHANGED';
+const PRODUCTS_IN_CART = 'PRODUCTS_IN_CART';
 
 // == Reducer
 const reducer = (state = initialState, action = {}) => {
@@ -21,6 +23,11 @@ const reducer = (state = initialState, action = {}) => {
       return {
         ...state,
         search: action.value,
+      };
+    case PRODUCTS_IN_CART:
+      return {
+        ...state,
+        cartProducts: action.products,
       };
     default:
       return state;
@@ -40,6 +47,11 @@ export const homeProductsFetched = (products) => ({
 export const searchInputChanged = (value) => ({
   type: SEARCH_INPUT_CHANGED,
   value,
+});
+
+export const productsInCart = (products) => ({
+  type: PRODUCTS_IN_CART,
+  products,
 });
 
 // == Export
