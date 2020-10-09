f3180a5659fab4b6c9ec9d3443c35428a2044985
Abibibi <abeba.ngwe@gmail.com>
2020-10-09 08:31:01 +0200

    add / remove from cart button optimized, logic and ccss wise

--- src/components/HomeProduct/index.js
+++ src/components/HomeProduct/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 
 import HomeProductStyled from './HomeProduct';
 
@@ -10,25 +11,24 @@ const HomeProduct = ({
   cartChanges,
   cartProducts,
 }) => {
+  const productInCart = cartProducts.find((productTitle) => productTitle === title);
+
+  const cartButtonText = productInCart ? 'Retirer du panier' : 'Ajouter au panier';
+
+  const buttonClass = classNames({
+    'cart-remove': productInCart,
+  });
+
   const addRemoveProduct = ({ target: { id: clickedProductTitle } }) => {
     let updatedCartProducts;
 
-    console.log('clicked title' + clickedProductTitle);
-
     if (cartProducts.length) {
-      cartProducts.map((titleToSeekFor) => {
-        console.log('title to seek for' + titleToSeekFor);
-        if (clickedProductTitle === titleToSeekFor) {
-          updatedCartProducts = cartProducts.filter((titleToKeep) => titleToKeep !== clickedProductTitle);
-          console.log('removed products' + updatedCartProducts);
-        }
-        else if (clickedProductTitle !== titleToSeekFor) {
-          updatedCartProducts = [...cartProducts, clickedProductTitle];
-          console.log('added products' + updatedCartProducts);
-        }
-        updatedCartProducts = cartProducts.filter((titleToKeep) => titleToKeep !== titleToKeep);
-        return updatedCartProducts;
-      });
+      if (productInCart) {
+        updatedCartProducts = cartProducts.filter((anyTitle) => anyTitle !== clickedProductTitle);
+      }
+      else {
+        updatedCartProducts = [...cartProducts, clickedProductTitle];
+      }
     }
     else {
       updatedCartProducts = [clickedProductTitle];
@@ -37,16 +37,19 @@ const HomeProduct = ({
     cartChanges(updatedCartProducts);
   };
 
-  const productInCart = cartProducts ? cartProducts.find(({ title: productTitle }) => productTitle === title) : false;
-
-  const cartButtonText = productInCart ? 'Retirer du panier' : 'Ajouter au panier';
-
   return (
     <HomeProductStyled>
       <img src={cover} alt={title} />
       <div>{price}€</div>
       <h3>{title}</h3>
-      <button id={title} type="submit" onClick={addRemoveProduct}>{cartButtonText}</button>
+      <button
+        type="submit"
+        id={title}
+        className={buttonClass}
+        onClick={addRemoveProduct}
+      >
+        {cartButtonText}
+      </button>
     </HomeProductStyled>
   );
 };
